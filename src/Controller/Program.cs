fb8bc3d535726d49258a7dc5f61d8bb3d3f83e3f
Twiddio <phaskins03@icloud.com>
2021-05-22 00:14:34 -0400

    commit

---
+++ src/Controller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arm.Main
+{
+    public class Controller
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
